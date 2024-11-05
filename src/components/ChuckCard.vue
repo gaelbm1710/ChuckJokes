d8270952209f62bb7a828d3167bbd20667cb43f7
gaelbm1710 <gaelmaldonado867@gmail.com>
2024-11-04 19:56:38 -0600

    Task10

---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card h-100">
+    <img :src="icon_url" class="card-img-top" alt="Chuck Norris Icon" />
+    <div class="card-body">
+      <p class="card-text">{{ value }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  icon_url: String,
+  value: String,
+});
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+}
+.card-img-top {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  margin: 15px auto 0;
+}
+.card-text {
+  font-size: 1rem;
+  text-align: center;
+}
+</style>
--- src/components/ChuckJokes.vue
+++ src/components/ChuckJokes.vue
@@ -1,24 +1,44 @@
 <template>
-  <div>
+  <div class="container">
     <h2>Chuck Norris Jokes</h2>
-    <ul>
-      <li v-for="joke in jokes" :key="joke.value">{{ joke.value }}</li>
-    </ul>
+    <div class="row">
+      <chuck-card
+        v-for="joke in jokes"
+        :key="joke.value"
+        :icon_url="joke.icon_url"
+        :value="joke.value"
+        class="col-lg-4 col-md-6 col-sm-12 mb-4"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import ChuckCard from "./ChuckCard.vue";
 
 const jokes = ref([
-  { value: "Chuck Norris can skydive into outer space." },
-  { value: "The chief export of Chuck Norris is pain." },
   {
+    icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+    value: "Chuck Norris can skydive into outer space.",
+  },
+  {
+    icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+    value: "The chief export of Chuck Norris is pain.",
+  },
+  {
+    icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
     value:
       "Chuck Norris doesn't read books. He stares them down until he gets the information he wants.",
   },
-  { value: "Time waits for no man. Unless that man is Chuck Norris." },
-  { value: "If you spell Chuck Norris in Scrabble, you win. Forever." },
+  {
+    icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+    value: "Time waits for no man. Unless that man is Chuck Norris.",
+  },
+  {
+    icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+    value: "If you spell Chuck Norris in Scrabble, you win. Forever.",
+  },
 ]);
 </script>
 
@@ -28,22 +48,5 @@ h2 {
   color: #333;
   text-align: center;
   margin-top: 1rem;
-}
-ul {
-  list-style: none;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-li {
-  background-color: #f3f3f3;
-  padding: 1rem;
-  margin: 0.5rem 0;
-  border-radius: 5px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  width: 80%;
-  max-width: 500px;
-  text-align: center;
 }
 </style>
